23f159cad9b27c22fb740813bf1786dac0161193
MingyuLian <142514410+MingyuLian@users.noreply.github.com>
2025-03-07 14:07:44 +0800

    remove unused voice app and related files; add users app with initial models, views, and admin configuration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # Import the include function
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('voice/', include('voice.urls')),  # Include the voice app's URLs
 ]
--- voice/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def test_api(request):
-    return Response({'message': 'Voice API is working!'})
-# Create your views here.
